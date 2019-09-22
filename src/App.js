ea722144d87522e6a09acf5f97ab0d4fbcae57d9
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-09-21 21:00:31 -0400

    destructuring the state

--- src/App.js
+++ src/App.js
@@ -1,65 +1,63 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import {Loading} from './Loading';
-
+import React, { Component } from "react";
+import axios from "axios";
+import { Loading } from "./Loading";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
-      loading: false,
+      loading: false
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
-
-  };
+  }
 
   getUsers() {
     this.setState({
-      loading: true,
+      loading: true
     });
 
-    axios(`https://api.randomuser.me/?nat=US&results=5`)
-    .then((response)=>this.setState({
-      users: [...this.state.users, ...response.data.results],
-      loading: false
-
-
-    }));
-
-
-  };
+    axios(`https://api.randomuser.me/?nat=US&results=5`).then(response =>
+      this.setState({
+        users: [...this.state.users, ...response.data.results],
+        loading: false
+      })
+    );
+  }
 
   handleSubmit(e) {
     e.preventDefault();
     this.getUsers();
-    console.log('More users loaded');
+    console.log("More users loaded");
   }
 
   componentWillMount() {
     this.getUsers();
-  };
-
-  
-
+  }
 
   render() {
+    const { loading, users } = this.state;
     return (
       <div className="container">
-        {!this.state.loading ? this.state.users.map(user => 
-          <div>
-            <h3>{user.name.first} </h3>
-            <h6>{user.email} </h6>
-            <hr/>
-            <form  onSubmit={this.handleSubmit}>
-              <input type="submit" value="load users" />
-            </form>
-            </div>): (<Loading message = "Hello this page is loading....."/>)}
+        <form onSubmit={this.handleSubmit}>
+          <input type="submit" value="load users" />
+        </form>
+        <hr/>
+        {!loading ? (
+          users.map(user => (
+            <div key={user.id.value}>
+              <h3 style={{ color: "red" }}>{user.name.first} </h3>
+              <h6>{user.email} </h6>
+              <hr/>
+            </div>
+          ))
+        ) : (
+            <Loading message="Hello this page is loading....." />
+          )}
       </div>
     );
   }
-
 }
 
 export default App;
